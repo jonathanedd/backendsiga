a9b9a7c574578f8827bc35a7de83e0178fdf0d45
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2024-09-01 15:58:36 -0500

    Adding env variables to database conection

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const db = new Sequelize("siga", "postgres", "92171002", {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-  define: {
-    timestamps: false,
-  },
-});
+const db = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: "postgres",
+    logging: false,
+    define: {
+      timestamps: false,
+    },
+  }
+);
 
 module.exports = db;
